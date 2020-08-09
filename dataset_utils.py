fd45b7edd2c19151af7ad3ee6f357bfdbebe46b0
Ali Maher <alimahera2a@gmail.com>
2020-08-09 19:20:55 +0200

    new commit

--- dataset_utils.py
+++ dataset_utils.py
@@ -1,19 +1,20 @@
 import numpy as np
 import os
 
-def merge_dataset(dataset_path, output_path ):
+def merge_dataset(dataset_path, output_path , name = 'dataset'):
     data_set = None
     for npy_file in os.listdir(dataset_path):
-        if npy_file.endswith(".npy")  and data_set != None:
-            data_sample = np.load(npy_file)
-            data_set = np.vstack(data_set, data_sample)
-        elif npy_file.endswith(".npy") and data_set == None:
-            data_sample = np.load(npy_file)
+        if npy_file.endswith(".npy")  and data_set is None :
+            data_sample = np.load(dataset_path+npy_file)
             data_set = data_sample
+        elif npy_file.endswith(".npy") :
+
+            data_sample = np.load(dataset_path + npy_file)
+            data_set = np.vstack((data_set, data_sample))
         else:
             continue
     np.save(output_path + '/' "dataset.npy", data_set)
-
-def get_sequence(x_path_positive,x_path_negative):
-
-    return X,y
+merge_dataset('output/nv/','dataset')
+# def get_sequence(x_path_positive,x_path_negative):
+#
+#     return X,y
